df52619680b72e70da64d58c6aef27ef5e95b51c
icramnibor <68248738+icramnibor@users.noreply.github.com>
2026-02-01 14:18:22 +0100

    LIN_ADVANCE tune
    Updated the ADVANCE_K constant from 0.03 to 0.04 in Configuration_adv.h to adjust the default compression length for all extruders. This may improve extrusion accuracy depending on filament and printer setup.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2444,7 +2444,7 @@
   #if ENABLED(DISTINCT_E_FACTORS)
     #define ADVANCE_K { 0.085 }    // (mm) Compression length per 1mm/s extruder speed, per extruder. Override with 'M900 T<tool> K<mm>'.
   #else
-    #define ADVANCE_K 0.03        // (mm) Compression length for all extruders. Override with 'M900 K<mm>'.
+    #define ADVANCE_K 0.04        // (mm) Compression length for all extruders. Override with 'M900 K<mm>'.
   #endif
   //#define ADVANCE_K_EXTRA       // Add a second linear advance constant, configurable with 'M900 L'.
 #endif
